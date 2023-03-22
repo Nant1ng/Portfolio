598259da5c2879f0ab8077f45b5c6c3dacbceaf8
Andrés <andressantana99@hotmail.se>
2023-03-22 13:00:16 +0100

    Added some style to Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import { SocialIcon } from "react-social-icons";
 
 function Header() {
   return (
-    <header>
+    <header className="sticky top-0 p-5 first-letter:flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <div className="flex flex-row items-center">
         <SocialIcon
           url="https://github.com/Nant1ng"
@@ -11,15 +11,21 @@ function Header() {
           bgColor="transparent"
         />
         <SocialIcon
-          url="https://github.com/Nant1ng"
+          url="https://www.linkedin.com/in/andr%C3%A9s-santana-5414981bb/"
           fgColor="gray"
           bgColor="transparent"
         />
+      </div>
+      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
         <SocialIcon
-          url="https://github.com/Nant1ng"
+          className="cursor-pointer"
+          network="email"
           fgColor="gray"
           bgColor="transparent"
         />
+        <p className="hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
       </div>
     </header>
   );
