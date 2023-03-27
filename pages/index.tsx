91448d5d08f240583ad4fe8a6f04a07a45017edb
Andrés <andressantana99@hotmail.se>
2023-03-27 17:51:55 +0200

    Added Experience

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Experience from "@/components/Experience";
 
 export default function Home() {
   return (
@@ -17,6 +18,9 @@ export default function Home() {
         </section>
         <section id="about" className="snap-center">
           <About />
+        </section>
+        <section id="experience" className="snap-center">
+          <Experience />
         </section>
       </div>
     </>
