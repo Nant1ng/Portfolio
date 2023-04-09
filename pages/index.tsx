036b37e2e4f6ad4353fdff2caba7446c9e289e2f
Andrés <andressantana99@hotmail.se>
2023-04-09 18:46:41 +0200

    Changed File name

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import About from "@/components/About";
-import Experience from "@/components/Experience";
+import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
 import ContactMe from "@/components/ContactMe";
@@ -25,7 +25,7 @@ export default function Home() {
           <About />
         </section>
         <section id="experience" className="snap-center">
-          <Experience />
+          <WorkExperience />
         </section>
         <section id="skills" className="snap-start">
           <Skills />
