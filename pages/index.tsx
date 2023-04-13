8fca6b79e7f30580926c6f4c5f6e04fd4750af3d
Andrés <andressantana99@hotmail.se>
2023-04-13 22:23:15 +0200

    Project done

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home({
           <Skills skills={skills} />
         </section>
         <section id="projects" className="snap-start">
-          <Projects />
+          <Projects projects={projects} />
         </section>
         <section id="contact" className="snap-start">
           <ContactMe socials={pageInfo} />
