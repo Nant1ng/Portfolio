12ab9d685e4a76cb57eeb22f8ab8afb331213080
Nant1ng <andressantana99@hotmail.se>
2024-02-13 08:40:25 +0100

    Done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,42 @@
+import Link from "next/link";
+import { DESKTOP_BP, TABLET_BP } from "../utils/breakpoints";
 import { SocialIcon } from "react-social-icons";
 import { motion } from "framer-motion";
-import Link from "next/link";
+import styled from "styled-components";
 import { Social } from "@/typings";
 
 type Props = {
   socials: Social[];
 };
 
+const Navbar = styled.header`
+  position: sticky;
+  top: 0;
+  padding: 1.25rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+  max-width: 80rem;
+  margin-left: auto;
+  margin-right: auto;
+  z-index: 10;
+  ${DESKTOP_BP} {
+    align-items: center;
+  }
+`;
+
+const ContactBtn = styled.p`
+  display: none;
+  ${TABLET_BP} {
+    display: inline-flex;
+    font-size: 0.875rem;
+    line-height: 1.25rem;
+  }
+`;
+
 function Header({ socials }: Props) {
   return (
-    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-10 xl:items-center">
+    <Navbar>
       <motion.div
         initial={{ x: -500, opacity: 0, scale: 0.5 }}
         animate={{ x: 0, opacity: 1, scale: 1 }}
@@ -20,8 +47,9 @@ function Header({ socials }: Props) {
           <SocialIcon
             key={social._id}
             url={social.url}
-            fgColor="gray"
+            fgColor="currentColor"
             bgColor="transparent"
+            className="text-gray-500 hover:text-gray-300"
             target="_blank"
           />
         ))}
@@ -30,21 +58,19 @@ function Header({ socials }: Props) {
         initial={{ x: 500, opacity: 0, scale: 0.5 }}
         animate={{ x: 0, opacity: 1, scale: 1 }}
         transition={{ duration: 1.5 }}
-        className="flex flex-row items-center text-gray-300"
+        className="flex flex-row items-center text-gray-500 hover:text-gray-300"
       >
         <SocialIcon
           url="#contact"
           network="email"
-          fgColor="gray"
+          fgColor="currentColor"
           bgColor="transparent"
         />
         <Link href="#contact">
-          <p className="hidden md:inline-flex text-sm text-gray-400">
-            Get In Touch
-          </p>
+          <ContactBtn>Get In Touch</ContactBtn>
         </Link>
       </motion.div>
-    </header>
+    </Navbar>
   );
 }
 
