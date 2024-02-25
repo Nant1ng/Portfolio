998bbf195bfe0bda6485539e50ce657d7062daa4
Nant1ng <andressantana99@hotmail.se>
2024-02-25 19:31:48 +0100

    Done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,61 +1,113 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useTypewriter, Cursor } from "react-simple-typewriter";
+
 import BackgroundCircles from "./BackgroundCircles";
-import { PageInfo } from "@/typings";
+import { LAPTOP_BP } from "../utils/breakpoints";
+
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { urlFor } from "@/sanity";
+import styled from "styled-components";
+
+import { PageInfo } from "@/typings";
 
 type Props = {
   pageInfo: PageInfo;
 };
 
+const HeroContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  overflow: hidden;
+`;
+
+const ZIndex = styled.div`
+  z-index: 20;
+`;
+
+const Role = styled.h2`
+  font-size: 1rem;
+  line-height: 1.25rem;
+  text-transform: uppercase;
+  color: #6b7280;
+  padding-bottom: 0.5rem;
+  letter-spacing: 10px;
+`;
+
+const TypeWriter = styled.h1`
+  font-size: 3rem;
+  line-height: 1;
+  font-weight: 600;
+  padding: 0 2.5rem;
+  ${LAPTOP_BP} {
+    font-size: 3.75rem;
+  }
+`;
+
+const LinkButtons = styled.div`
+  padding-top: 1.25rem;
+`;
+
 function Hero({ pageInfo }: Props) {
   const [text] = useTypewriter({
     words: [
       `Hi, The Name's ${pageInfo?.name}`,
-      "Guy-who-loves-to-workout.tsx",
+      "Self-Proclaimed-Master-Chef.tsx",
+      "Guy-Who-Loves-To-Workout.tsx",
       "<But-Loves-To-Code-More />",
     ],
     loop: true,
     delaySpeed: 2000,
+    typeSpeed: 50,
+    deleteSpeed: 50,
   });
 
   return (
-    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+    <HeroContainer>
       <BackgroundCircles />
       <Image
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src={urlFor(pageInfo?.heroImage).url()}
-        alt={`${pageInfo?.name} profile pic`}
+        alt={`Profile picture of ${pageInfo?.name}`}
         width={100}
         height={100}
         priority
       />
-      <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[10px]">
-          {pageInfo?.role}
-        </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
-          <span className="mr-3">
-            {text}<Cursor cursorColor="#6b0a7f" />
+      <ZIndex>
+        <Role>{pageInfo?.role}</Role>
+        <TypeWriter>
+          <span>
+            {text} <Cursor cursorColor="#6b0a7f" />
           </span>
-        </h1>
-        <div className="pt-5">
+        </TypeWriter>
+        <LinkButtons>
           <Link href="#about">
-            <button className="heroButton">About</button>
+            <button className="heroButton" aria-label="Learn more about me">
+              About
+            </button>
           </Link>
           <Link href="#experience">
-            <button className="heroButton">Experience</button>
+            <button className="heroButton" aria-label="View my work experience">
+              Experience
+            </button>
           </Link>
           <Link href="#skills">
-            <button className="heroButton">Skills</button>
+            <button className="heroButton" aria-label="Explore my skills">
+              Skills
+            </button>
           </Link>
           <Link href="#projects">
-            <button className="heroButton">Projects</button>
+            <button className="heroButton" aria-label="See my projects">
+              Projects
+            </button>
           </Link>
-        </div>
-      </div>
-    </div>
+        </LinkButtons>
+      </ZIndex>
+    </HeroContainer>
   );
 }
 
