ac31eea3d8670def3e10ff36e5663146fb2df7bf
Nant1ng <andressantana99@hotmail.se>
2024-03-01 20:15:25 +0100

    Added XL Desktop Breakpoint

--- utils/breakpoints.js
+++ utils/breakpoints.js
@@ -2,8 +2,10 @@ const MOBILE_MIN_WIDTH = 376;
 const TABLET_MIN_WIDTH = 768;
 const LAPTOP_MIN_WIDTH = 840;
 const DESKTOP_MIN_WIDTH = 1313;
+const XL_DESKTOP_MIN_WIDTH = 1536;
 
 export const MOBILE_BP = `@media (min-width: ${MOBILE_MIN_WIDTH}px)`;
 export const TABLET_BP = `@media (min-width: ${TABLET_MIN_WIDTH}px)`;
 export const LAPTOP_BP = `@media (min-width: ${LAPTOP_MIN_WIDTH}px)`;
 export const DESKTOP_BP = `@media (min-width: ${DESKTOP_MIN_WIDTH}px)`;
+export const XL_DESKTOP_BP = `@media (min-width: ${XL_DESKTOP_MIN_WIDTH}px)`;
